13c8933c1ecb0cc947bb9cbd9d98a0dea1a601f9
Colin O'Dowd <cdodowdee@gmail.com>
2020-06-02 19:59:38 -0400

    IEX API working in terminal

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-const apiData = {
+const fetch = require("node-fetch");
+
+const apiVariables= {
     url: 'https://cloud.iexapis.com/stable/stock',
     ticker: 'AAPL',
     core: 'book',
-    token: '?pk_8bcad0d669594bdfb4fe4c79a8558d23'
+    token: '?token=pk_8bcad0d669594bdfb4fe4c79a8558d23'
 }
 
-const {url, ticker, core, token} = apiData
+const {url, ticker, core, token} = apiVariables
 const apiUrl = `${url}/${ticker}/${core}${token}`
 
 console.log(apiUrl)
 
 fetch(apiUrl)
-    .then( (data) => console.log(data.json()) )
+    .then(promise => promise.json())
+    .then(data => console.log(data))
