e128afaa7e7ceb265e765464bbba159c6327317a
Colin O'Dowd <cdodowdee@gmail.com>
2020-06-02 19:30:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const apiData = {
     url: 'https://cloud.iexapis.com/stable/stock',
     ticker: 'AAPL',
-    data: 'book',
+    core: 'book',
     token: '?pk_8bcad0d669594bdfb4fe4c79a8558d23'
 }
 
-const {url, ticker, data, token} = apiData
-const apiUrl = `${url}/${ticker}/${data}${token}`
+const {url, ticker, core, token} = apiData
+const apiUrl = `${url}/${ticker}/${core}${token}`
 
 console.log(apiUrl)
+
+fetch(apiUrl)
+    .then( (data) => console.log(data.json()) )
