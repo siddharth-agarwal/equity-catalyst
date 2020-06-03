09d4b062c3f0053ecaa919ff46345ebec98e4d09
Colin O'Dowd <cdodowdee@gmail.com>
2020-06-03 00:46:32 -0400

    works in terminal, next step react app

--- equity-catalyst/src/App.js
+++ equity-catalyst/src/App.js
@@ -2,6 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const fetch = require("node-fetch");
+
+const apiVariables= {
+    url: 'https://cloud.iexapis.com/stable/stock',
+    ticker: 'AAPL',
+    core: 'book',
+    token: '?token=pk_8bcad0d669594bdfb4fe4c79a8558d23'
+}
+
+const {url, ticker, core, token} = apiVariables
+const apiUrl = `${url}/${ticker}/${core}${token}`
+
+fetch(apiUrl)
+    .then(data => data.json()) //prints only the ticker
+    .then(data => {
+        data.trades.forEach(data => { //prints last 20 trade prices
+            console.log(data.price)
+        });
+
+        console.log(data.quote.symbol) //prints ticker
+    })
+
+
 function App() {
   return (
     <div className="App">
@@ -10,14 +33,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        data.quote.symbol 
       </header>
     </div>
   );
--- main.js
+++ main.js
@@ -3,21 +3,22 @@ const fetch = require("node-fetch");
 const apiVariables= {
     url: 'https://cloud.iexapis.com/stable/stock',
     ticker: 'AAPL',
-    core: 'book',
+    request: 'book',
     token: '?token=pk_8bcad0d669594bdfb4fe4c79a8558d23'
 }
 
-const {url, ticker, core, token} = apiVariables
-const apiUrl = `${url}/${ticker}/${core}${token}`
+const {url, ticker, request, token} = apiVariables
+const apiUrl = `${url}/${ticker}/${request}${token}`
 
 console.log(apiUrl)
 
 fetch(apiUrl)
     .then(data => data.json()) //prints only the ticker
     .then(data => {
-        data.trades.forEach(data => {
+        
+        data.trades.forEach(data => { //prints last 20 trade prices
             console.log(data.price)
         });
 
-        console.log(data.quote.symbol)
+        console.log(data.quote.symbol) //prints ticker
     })
