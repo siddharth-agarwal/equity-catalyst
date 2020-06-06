a3474d3761e6f409cd2087af2a6585d50cbcff7e
Colin O'Dowd <cdodowdee@gmail.com>
2020-06-06 13:51:23 -0400

    search bar and background images

--- equity-catalyst/src/App.js
+++ equity-catalyst/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 const fetch = require("node-fetch");
 
@@ -14,27 +12,18 @@ const apiVariables= {
 const {url, ticker, core, token} = apiVariables
 const apiUrl = `${url}/${ticker}/${core}${token}`
 
-fetch(apiUrl)
-    .then(data => data.json()) //prints only the ticker
-    .then(data => {
-        data.trades.forEach(data => { //prints last 20 trade prices
-            console.log(data.price)
-        });
-
-        console.log(data.quote.symbol) //prints ticker
-    })
-
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        data.quote.symbol 
-      </header>
+      <main>
+        <div className="search-box">
+          <input 
+            type = "text"
+            className = "search-bar"
+            placeholder="Search your ticker..."
+            />
+        </div>
+      </main>
     </div>
   );
 }
