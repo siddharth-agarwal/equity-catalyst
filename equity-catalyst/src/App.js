67e33ee9b59c35048e814ef338830554753fc2ca
Colin O'Dowd <cdodowdee@gmail.com>
2020-06-07 13:44:36 -0400

    React act displays ticker and price and has night mode after 5pm

--- equity-catalyst/src/App.js
+++ equity-catalyst/src/App.js
@@ -1,28 +1,54 @@
-import React from 'react';
-
-const fetch = require("node-fetch");
+import React, { useState } from 'react';
 
 const apiVariables= {
-    url: 'https://cloud.iexapis.com/stable/stock',
-    ticker: 'AAPL',
-    core: 'book',
-    token: '?token=pk_8bcad0d669594bdfb4fe4c79a8558d23'
+  url: 'https://cloud.iexapis.com/stable/stock',
+  request: 'book',
+  token: '?token=pk_8bcad0d669594bdfb4fe4c79a8558d23'
 }
 
-const {url, ticker, core, token} = apiVariables
-const apiUrl = `${url}/${ticker}/${core}${token}`
 
 function App() {
+
+  var d = new Date();
+  var hours = d.getHours();
+  var five_pm = 17;
+
+  const [ticker, setTicker] = useState('')
+  const [returnData, setReturnData] = useState({})
+  const search = evt => {
+    if (evt.key == "Enter") {
+      const apiUrl = `${apiVariables.url}/${ticker}/${apiVariables.request}${apiVariables.token}`
+
+      fetch(apiUrl)
+        .then(data => data.json()) 
+        .then(data => {
+          setReturnData(data)
+          setTicker('')
+        })
+      }
+  }
+
   return (
-    <div className="App">
+    <div className={((hours >= five_pm) ? 'App_night' : 'App')}>
       <main>
-        <div className="search-box">
+      <div className="search-box">
           <input 
-            type = "text"
-            className = "search-bar"
-            placeholder="Search your ticker..."
-            />
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setTicker(e.target.value)}
+            value={ticker}
+            onKeyPress={search}
+          />
+      </div>
+      {(typeof returnData.quote != "undefined") ? (
+        <div>
+          <div className="data-box">
+            <div className="symbolName">{returnData.quote.symbol}</div>
+            <div className="closePrice">{returnData.quote.close}</div>
+          </div>
         </div>
+        ) : ('')}
       </main>
     </div>
   );
