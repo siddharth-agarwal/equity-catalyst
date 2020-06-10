33fd9b7d5df4f713ec1448e285f6dea270e1aab9
siddharth agarwal <siddharth.a@outlook.com>
2020-06-09 21:16:49 -0400

    update css styling + minor syntax fixes to js

--- equity-catalyst/src/App.js
+++ equity-catalyst/src/App.js
@@ -10,17 +10,17 @@ const apiVariables= {
 function App() {
 
   var d = new Date();
-  var hours = d.getHours();
-  var five_pm = 17;
+  var FIVE_PM = 17, FOUR_AM = 4;
+  var is_night = (d.getHours() > FIVE_PM || d.getHours < FOUR_AM)
 
   const [ticker, setTicker] = useState('')
   const [returnData, setReturnData] = useState({})
   const search = evt => {
-    if (evt.key == "Enter") {
+    if (evt.key === "Enter") {
       const apiUrl = `${apiVariables.url}/${ticker}/${apiVariables.request}${apiVariables.token}`
 
       fetch(apiUrl)
-        .then(data => data.json()) 
+        .then(data => data.json())
         .then(data => {
           setReturnData(data)
           setTicker('')
@@ -29,13 +29,13 @@ function App() {
   }
 
   return (
-    <div className={((hours >= five_pm) ? 'App_night' : 'App')}>
+    <div className={((is_night) ? 'App_night' : 'App')}>
       <main>
       <div className="search-box">
-          <input 
+          <input
             type="text"
             className="search-bar"
-            placeholder="Search..."
+            placeholder="Enter a ticker to lookup..."
             onChange={e => setTicker(e.target.value)}
             value={ticker}
             onKeyPress={search}
@@ -43,10 +43,31 @@ function App() {
       </div>
       {(typeof returnData.quote != "undefined") ? (
         <div>
-          <div className="data-box">
-            <div className="symbolName">{returnData.quote.symbol}</div>
-            <div className="closePrice">{returnData.quote.close}</div>
-          </div>
+          <table className="data-box">
+            <thead>
+              <tr>
+                <th colSpan="2">{returnData.quote.symbol}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="symbolData">
+                <td>Company</td>
+                <td>{returnData.quote.companyName}</td>
+              </tr>
+              <tr className="symbolData">
+                <td>Closing Price</td>
+                <td>{returnData.quote.close != null ? "$"+returnData.quote.close : "n/a"}; (Source: {returnData.quote.closeSource})</td>
+              </tr>
+              <tr className="symbolData">
+                <td>Latest Price</td>
+                <td>${returnData.quote.latestPrice}; (Source: {returnData.quote.latestSource})</td>
+              </tr>
+              <tr className="symbolData">
+                <td>Primary Listed Exchange</td>
+                <td>{returnData.quote.primaryExchange}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
         ) : ('')}
       </main>
